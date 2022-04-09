bdb173b48b85252bda9d840859eb9de5c7565bb3
Luca Casonato <hello@lcas.dev>
2022-04-09 11:15:38 +0200

    chore: update deps (#7)

--- test_deps.ts
+++ test_deps.ts
@@ -1,6 +1,6 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.14.25/mod.js";
+import * as esbuild from "https://deno.land/x/esbuild@v0.14.34/mod.js";
 export { esbuild };
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.128.0/testing/asserts.ts";
+} from "https://deno.land/std@0.134.0/testing/asserts.ts";
