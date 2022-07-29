3b8c32c9eef1522a0d3ae28bed8914dcbf637456
Luca Casonato <hello@lcas.dev>
2022-07-30 00:03:22 +0200

    chore: bump dependencies (#19)

--- examples/bundle.ts
+++ examples/bundle.ts
@@ -1,9 +1,9 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.14.39/mod.js";
-import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.0/mod.ts";
+import * as esbuild from "https://deno.land/x/esbuild@v0.14.51/mod.js";
+import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.1/mod.ts";
 
 await esbuild.build({
   plugins: [denoPlugin()],
-  entryPoints: ["https://deno.land/std@0.139.0/hash/sha1.ts"],
+  entryPoints: ["https://deno.land/std@0.150.0/hash/sha1.ts"],
   outfile: "./dist/sha1.esm.js",
   bundle: true,
   format: "esm",
