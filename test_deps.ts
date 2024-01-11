3fed84ca4b79f5705b8c7c27c3846005656265ff
Reed von Redwitz <reed+github@vonredwitz.com>
2024-01-11 09:47:11 +0100

    chore: bump dependencies (#99)
    * chore: bump deps
    
    * also bump readme

--- test_deps.ts
+++ test_deps.ts
@@ -1,9 +1,9 @@
-import * as esbuildNative from "https://deno.land/x/esbuild@v0.19.2/mod.js";
-import * as esbuildWasm from "https://deno.land/x/esbuild@v0.19.2/wasm.js";
+import * as esbuildNative from "https://deno.land/x/esbuild@v0.19.11/mod.js";
+import * as esbuildWasm from "https://deno.land/x/esbuild@v0.19.11/wasm.js";
 export { esbuildNative, esbuildWasm };
 export {
   assert,
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.201.0/assert/mod.ts";
-export { join } from "https://deno.land/std@0.201.0/path/mod.ts";
+} from "https://deno.land/std@0.211.0/assert/mod.ts";
+export { join } from "https://deno.land/std@0.211.0/path/mod.ts";
