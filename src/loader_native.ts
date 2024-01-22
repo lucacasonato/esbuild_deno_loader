5d47221e2c4e1729e2c13c80516e787bcc7698b4
Nicolas Gasull <nicolas@gasull.net>
2024-01-22 13:30:59 +0100

    fix: run tmp operations in same file system as DENO_DIR (#79)
    Co-authored-by: Luca Casonato <hello@lcas.dev>

--- src/loader_native.ts
+++ src/loader_native.ts
@@ -117,6 +117,7 @@ export class NativeLoader implements Loader {
       name,
     );
     const linkDirParent = dirname(linkDir);
+    const tmpDirParent = join(DENO_DIR.root, "deno_esbuild_tmp");
 
     // check if the package is already linked, if so, return the link and skip
     // a bunch of work
@@ -130,7 +131,8 @@ export class NativeLoader implements Loader {
 
     // create a temporary directory, recursively hardlink the package contents
     // into it, and then rename it to the final location
-    const tmpDir = await Deno.makeTempDir();
+    await Deno.mkdir(tmpDirParent, { recursive: true });
+    const tmpDir = await Deno.makeTempDir({ dir: tmpDirParent });
     await linkRecursive(packageDir, tmpDir);
     try {
       await Deno.mkdir(linkDirParent, { recursive: true });
